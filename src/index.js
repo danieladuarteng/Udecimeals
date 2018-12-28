32eca154e1d1e75e7008a130a290630901950d88
Daniela Duarte <falecom@danieladuarte.com.br>
2018-12-28 10:25:32 -0200

    chore: add react-redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './reducers';
 
 //criando a store
